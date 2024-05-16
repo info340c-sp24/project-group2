b2b006ea97f163c343074391f223381198684606
netrak26 <netrak@uw.edu>
2024-05-16 00:37:41 -0700

    added components folder, and messaging js file

---
+++ src/messaging.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <HomePage />
+      {/* <CalendarPage /> */}
+      {/* <MessagingPage /> */}
+      {/* <MediaPage /> */}
+    </div>
+  );
+}
+
+export default App;
