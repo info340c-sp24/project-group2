b5f3aa390180bf9b3a78878a349fd358740f5c80
nkanna <nkanna@uw.edu>
2024-05-17 11:17:53 -0700

    pushing changes

--- src/pages/media.js
+++ src/pages/media.js
@@ -52,7 +52,12 @@ function RecentUploads({ recentUploads }) {
             </div>
           ))
         ) : (
-          <p>No recent uploads.</p>
+          <>
+            <div className="folder"><i className="material-icons">folder</i><h3>Week 1</h3></div>
+            <div className="folder"><i className="material-icons">folder</i><h3>Week 2</h3></div>
+            <div className="folder"><i className="material-icons">folder</i><h3>Week 3</h3></div>
+            <div className="folder"><i className="material-icons">folder</i><h3>Week 4</h3></div>
+          </>
         )}
       </div>
     </section>
@@ -72,7 +77,12 @@ function OlderFiles({ olderFiles }) {
             </div>
           ))
         ) : (
-          <p>No older files.</p>
+          <>
+            <div className="folder"><i className="material-icons">folder</i><h3>Winter 2023</h3></div>
+            <div className="folder"><i className="material-icons">folder</i><h3>Spring 2023</h3></div>
+            <div className="folder"><i className="material-icons">folder</i><h3>Fall 2023</h3></div>
+            <div className="folder"><i className="material-icons">folder</i><h3>Winter 2024</h3></div>
+          </>
         )}
       </div>
     </section>
@@ -86,8 +96,4 @@ function Footer() {
     </footer>
   );
 }
-
-
-
-
 
