92553129a64116f8cb5a42a37d30252191a9775d
netrak26 <netrak@uw.edu>
2024-05-17 11:51:44 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import MessagingPage from './pages/messaging';
 import './styles/messaging.css';
 import Login from './pages/login';
 import Media from './pages/media';
-import './styles/login.css';
-import './styles/media.css';
+//import './styles/login.css';
+//import './styles/media.css';
 
 function App(props) {
   return (
-    <div>
-      {/* <LoginPage /> */}
-      {/* <HomePage /> */}
-      {/* <CalendarPage /> */}
-      <MessagingPage />
-      {/* <MediaPage /> */}
-    </div>
+    <>
+      <div>
+        {/* <LoginPage /> */}
+        {/* <HomePage /> */}
+        {/* <CalendarPage /> */}
+        <MessagingPage />
+        {/* <MediaPage /> */}
+      </div>
+      <footer>
+      <p>
+        Copyright <span>©</span> 2024 Group 2. All rights reserved.
+      </p>
+      </footer>
+    </>
   );
 }
 
