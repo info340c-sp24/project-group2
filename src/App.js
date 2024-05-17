814d2cfc8ed8821eb94059d83eafe35902c10f37
netrak26 <netrak@uw.edu>
2024-05-17 12:18:50 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MessagingPage from './pages/messaging';
 import './styles/messaging.css';
-import Login from './pages/login';
-import Media from './pages/media';
+import LoginPage from './pages/login';
+import MediaPage from './pages/media';
 import './styles/login.css';
 import './styles/media.css';
 
-<<<<<<< HEAD
 function App(props) {
-=======
-function App() {
-  const loginProps = {
-    title: 'RSO Communication Platform',
-    subtitle: 'Please login with your UW Net ID',
-  };
-
-  const mediaProps = {
-    recentUploads: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-    olderFiles: ['Winter 2023', 'Spring 2023', 'Fall 2023', 'Winter 2024'],
-  };
-
->>>>>>> 7361b1b2ab335390af03e875e21f6b80ee20a129
   return (
-    <div>
-      {/* <LoginPage /> */}
-      {/* <HomePage /> */}
-      {/* <CalendarPage /> */}
-      <MessagingPage />
-      {/* <MediaPage /> */}
-    </div>
+    <>
+      <div>
+        {/* <LoginPage /> */}
+        {/* <HomePage /> */}
+        {/* <CalendarPage /> */}
+        <MessagingPage />
+        {/* <MediaPage /> */}
+      </div>
+      <footer>
+      <p>
+        Copyright <span>©</span> 2024. All rights reserved.
+      </p>
+      </footer>
+  </>
   );
 }
 
