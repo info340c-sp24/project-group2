89dff9ba1a00670b8f8aa457655d77652e14870b
ChillarneyMan <arney.chillarney@gmail.com>
2024-06-03 21:29:49 -0700

    fixed merge comflixt

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import './firebaseConfig';
 import { createClient } from '@supabase/supabase-js'
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const supabase = createClient(
   "https://qlquwbkuupywrvgppbiw.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFscXV3Ymt1dXB5d3J2Z3BwYml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0Mzc4NTAsImV4cCI6MjAzMzAxMzg1MH0.1Fx6OcSpN2WuSMGK9qqPFjQ-OEbHGJFyRSSCg7QoxTs"
 );
-import 'bootstrap/dist/css/bootstrap.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
